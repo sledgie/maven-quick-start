af2d9fb623a2f512fdba32f237bde75253c0d3c0
acorpuz <abner.corpuz@hotmail.com>
2020-02-13 13:14:35 -0500

    Changing compiler settings to 1.8

--- src/main/java/com/itechtions/Application.java
+++ src/main/java/com/itechtions/Application.java
@@ -1,12 +1,25 @@
 package com.itechtions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application{
-	public Application(){
+	public void greet(){
+		List<String> greetings = new ArrayList<>();
+		greetings.add("Hello!");
+		
+		for(String greeting : greetings){
+			System.out.println("Greeting: " + greeting);
+		}
+	}
+	
+	public Application(){		
 		System.out.println("Inside Application");
 	}
 	
 	public static void main(String[] args){
 		System.out.println("Starting application");
 		Application app = new Application();
+		app.greet();
 	}
 }
